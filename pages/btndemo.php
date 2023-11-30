c5ac17c77159f6140570bc6d1adb8cca2347f275
Nguyễn Quang Hiếu <91005869+nguynqh@users.noreply.github.com>
2023-11-30 15:25:53 +0700

    Add files via upload

---
+++ pages/btndemo.php
@@ -0,0 +1,11 @@
+<link rel="stylesheet" href="../css/button.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<div class="Btn">
+    <span class="btnIcon">
+        <i class="fa fa-plus"></i>
+    </span>
+    <span class="text">Copy</span>
+</div>
+<div class="add-bolt">
+    <img src="../image/bolt-removebg-preview.png" alt="bolt_icon" class="bolt-icon">
+</div>
---
+++ pages/properties_form.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/navbar_sidebar.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="../css/properties_form.css">
+    <title>Properties</title>
+</head>
+<body>
+    <div class="container-scroller">
+        <?php include '../base/navbar.php'; ?>
+        <div class="page-body-wraper">
+            <?php include '../base/sidebar.php'; ?>
+            <div class="main-content-wrap">
+                <div class="categories_properties">
+                    <div class="main-label item">Them (Xoa,Xem,Sua)</div>
+                    <!-- ID hide when in mode Add-->
+                    <div class="product-id item" style="display: none;">
+                        <h3>Id:</h3>
+                        <input type="text" class="input-id" placeholder="C45902" >
+                    </div>
+                    <!-- NAME -->
+                    <div class="product-name item">
+                        <h3>Name:</h3>
+                        <input type="text" class="input-name" placeholder="Text your category name s">
+                    </div>
+                    <!-- COLOR -->
+                    <div class="product-color item">
+                        <h3>Color:</h3>
+                        <input type="color" class="input-color" id="colorPicker">
+                        <h3 id="colorCode">Your color code: please choose your color</h3>
+                    </div>
+                    <!-- CURRENT PRICE -->
+                    <div class="product-current-price item">
+                        <h3 style="font-weight: 500;">Price:</h3>
+                        <input type="text" name="" id="" class="" placeholder="Enter category price">
+                    </div>
+                    <!-- QUANTITY -->
+                    <div class="product-quantity item" style="display: block;">
+                        <!-- the number of bolt of this category in warehouse -->
+                        <h3 id="quantity">Quantity: 0</h3>
+                        <div class="add-bolt-container" id="formAddBolt">
+                            <!-- <div class="add_bolt">
+                                <span class="id-bolt" id="idBolt">1.</span>
+                                <img src="../image/bolt-removebg-preview.png" alt="" class="botl-icon">
+                                <input type="text" placeholder="length" id="lengthOfBolt" style="width: 52px;">
+                                <span>m</span>
+                            </div>  -->
+                        </div>
+                        <div class="action-button-bolt-form">
+                            <button id="addBolt">Add a bolt</button>
+                            <button id="delBolt">Del</button>
+                        </div>
+                    </div>
+                    <!-- SUBMIT BUTTON -->
+                    <div class="submit-button" style="padding-left: 40%;">
+                        <div class="Btn">
+                            <span class="btn-icon">
+                                <i class="fa fa-check"></i>
+                            </span>
+                            <span class="text">Create</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="../js/properties_form.js"></script>
+
+</body>
+</html>
