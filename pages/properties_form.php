c7ee814aa528772829f7cc1fcb33416bdfd31acc
Nguyễn Quang Hiếu <91005869+nguynqh@users.noreply.github.com>
2023-12-02 19:15:36 +0700

    Add files via upload

--- pages/properties_form.php
+++ pages/properties_form.php
@@ -16,52 +16,69 @@
             <?php include '../base/sidebar.php'; ?>
             <div class="main-content-wrap">
                 <div class="categories_properties">
-                    <div class="main-label item">Them (Xoa,Xem,Sua)</div>
-                    <!-- ID hide when in mode Add-->
-                    <div class="product-id item" style="display: none;">
-                        <h3>Id:</h3>
-                        <input type="text" class="input-id" placeholder="C45902" >
-                    </div>
-                    <!-- NAME -->
-                    <div class="product-name item">
-                        <h3>Name:</h3>
-                        <input type="text" class="input-name" placeholder="Text your category name s">
-                    </div>
-                    <!-- COLOR -->
-                    <div class="product-color item">
-                        <h3>Color:</h3>
-                        <input type="color" class="input-color" id="colorPicker">
-                        <h3 id="colorCode">Your color code: please choose your color</h3>
-                    </div>
-                    <!-- CURRENT PRICE -->
-                    <div class="product-current-price item">
-                        <h3 style="font-weight: 500;">Price:</h3>
-                        <input type="text" name="" id="" class="" placeholder="Enter category price">
-                    </div>
-                    <!-- QUANTITY -->
-                    <div class="product-quantity item" style="display: block;">
-                        <!-- the number of bolt of this category in warehouse -->
-                        <h3 id="quantity">Quantity: 0</h3>
-                        <div class="add-bolt-container" id="formAddBolt">
-                            <!-- <div class="add_bolt">
-                                <span class="id-bolt" id="idBolt">1.</span>
-                                <img src="../image/bolt-removebg-preview.png" alt="" class="botl-icon">
-                                <input type="text" placeholder="length" id="lengthOfBolt" style="width: 52px;">
-                                <span>m</span>
-                            </div>  -->
+                    <div class="main-label item">Add a category <!--(Xoa,Xem,Sua)--></div>
+                    <div class="form-add-cate" style="padding-left: 20px">              
+                        <!-- ID hide when in mode Add-->
+                        <div class="product-id item" style="display: none;">
+                            <h3>Id:</h3>
+                            <input type="text" class="input-id" placeholder="C45902" >
                         </div>
-                        <div class="action-button-bolt-form">
-                            <button id="addBolt">Add a bolt</button>
-                            <button id="delBolt">Del</button>
+                        <!-- NAME -->
+                        <div class="product-name item">
+                            <h3>Name:</h3>
+                            <input type="text" class="" placeholder="Text your category name">
                         </div>
-                    </div>
-                    <!-- SUBMIT BUTTON -->
-                    <div class="submit-button" style="padding-left: 40%;">
-                        <div class="Btn">
-                            <span class="btn-icon">
-                                <i class="fa fa-check"></i>
-                            </span>
-                            <span class="text">Create</span>
+                        <!-- COLOR -->
+                        <div class="product-color item">
+                            <h3>Color:</h3>
+                            <input type="color" class="input-color" id="colorPicker">
+                            <h3 id="colorCode" >Your color code: </h3>
+                            <h3 id="selectedColor" style="padding-left: 3px; color: #7c858b" >please choose your color</h3>
+                        </div>
+                        <!-- CURRENT PRICE -->
+                        <div class="product-current-price item">
+                            <h3 style="font-weight: 500;">Price:</h3>
+                            <input type="text" name="" id="inPutPrice" class="" placeholder="Enter category price" style="width: 165px;">
+                            <h3>đ</h3>
+                        </div>
+                        <!-- SUPPLIER -->
+                        <div class="product-supplier item">
+                            <h3>Chọn nhà cung cấp:</h3>
+                            <select name="" id="selectSupplier" class="select-supplier" >
+                                <option value="default" disabled hidden selected >Select a supplier</option>
+                                <option value="value1">hieu</option>
+                                <option value="value2">duc</option>
+                                <option value="value3">haas</option>
+                                <option value="value1">hieu</option>
+                                <option value="value2">duc</option>
+                                <option value="value3">haas</option>
+                            </select>
+                        </div>
+                        <!-- QUANTITY -->
+                        <div class="product-quantity item" style="display: block;">
+                            <!-- the number of bolt of this category in warehouse -->
+                            <h3 id="quantity">Quantity: 0</h3>
+                            <div class="add-bolt-container" id="formAddBolt">
+                                <!-- <div class="add_bolt">
+                                    <span class="id-bolt" id="idBolt">1.</span>
+                                    <img src="../image/bolt-removebg-preview.png" alt="" class="botl-icon">
+                                    <input type="text" placeholder="length" id="lengthOfBolt" style="width: 52px;">
+                                    <span>m</span>
+                                </div>  -->
+                            </div>
+                            <div class="action-button-bolt-form">
+                                <button id="addBolt" class="button-action">Add a bolt</button>
+                                <button id="delBolt" class="button-action">Del</button>
+                            </div>
+                        </div>
+                        <!-- SUBMIT BUTTON -->
+                        <div class="submit-button" style="padding-left: 40%;">
+                            <div class="Btn">
+                                <span class="btn-icon">
+                                    <i class="fa fa-check"></i>
+                                </span>
+                                <span class="text">Create</span>
+                            </div>
                         </div>
                     </div>
                 </div>
